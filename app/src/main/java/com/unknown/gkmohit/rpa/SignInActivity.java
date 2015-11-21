35d0223a4d68413606209298a83e708bc90c6706
Mohit Kishore <gkmohit@gmail.com>
2015-11-21 11:14:57 -0500

    Mohit - Added homepage fragement activity

--- app/src/main/java/com/unknown/gkmohit/rpa/SignInActivity.java
+++ app/src/main/java/com/unknown/gkmohit/rpa/SignInActivity.java
@@ -1,8 +1,10 @@
 package com.unknown.gkmohit.rpa;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,6 +27,14 @@ public class SignInActivity extends AppCompatActivity {
         Typeface font = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
         mUserNameFA.setTypeface(font);
         mPasswordFA.setTypeface(font);
+
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homePage = new Intent(SignInActivity.this, HomePageActivity.class);
+                SignInActivity.this.startActivity(homePage);
+            }
+        });
     }
 
 }
