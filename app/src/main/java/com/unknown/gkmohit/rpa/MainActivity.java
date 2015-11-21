25f77bb806fd332960c97f179953403eee8fcc85
Mohit Kishore <gkmohit@gmail.com>
2015-11-20 21:14:39 -0500

    Added font-awesome, added signInActivity, created sign in screen.

--- app/src/main/java/com/unknown/gkmohit/rpa/MainActivity.java
+++ app/src/main/java/com/unknown/gkmohit/rpa/MainActivity.java
@@ -1,13 +1,31 @@
 package com.unknown.gkmohit.rpa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.signInButton)
+    Button mSignInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this, SignInActivity.class);
+                MainActivity.this.startActivity(signIn);
+            }
+        });
     }
 }
