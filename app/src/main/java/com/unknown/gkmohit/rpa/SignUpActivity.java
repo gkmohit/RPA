57691482488ca145562e5fee18f738d75a91d432
joy99 <sadman.hasan99@live.com>
2015-11-21 17:39:46 -0500

    test

--- app/src/main/java/com/unknown/gkmohit/rpa/SignUpActivity.java
+++ app/src/main/java/com/unknown/gkmohit/rpa/SignUpActivity.java
@@ -1,25 +1,57 @@
 package com.unknown.gkmohit.rpa;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
  * Created by Shrey on 11/20/2015.
  */
+
+
 public class SignUpActivity extends AppCompatActivity {
+
+    @Bind(R.id.signUpButton)
+    Button mSignUpButton;
+
+    @Bind(R.id.checkBox)
+    CheckBox cb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_sign_up);
+
+
         ButterKnife.bind(this);
-      //  Typeface font = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
-      //  mUserNameFA.setTypeface(font);
-       // mPasswordFA.setTypeface(font);
 
+        //mSignUpButton.setVisibility(View.INVISIBLE);
+        mSignUpButton.setActivated(false);
 
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mSignUpButton.setVisibility(View.VISIBLE);
+                mSignUpButton.setActivated(true);
+            }
+        });
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homePage = new Intent(SignUpActivity.this, HomePageActivity.class);
+                SignUpActivity.this.startActivity(homePage);
+            }
+        });
     }
+
 }
+
