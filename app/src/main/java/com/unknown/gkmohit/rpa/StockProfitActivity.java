f1dc81ec7516ace3c8ba12534d61a370ca03054f
Shrey <shreyasvb93@gmail.com>
2015-11-22 04:49:32 -0500

    Shrey activated Stocks Goals and Profit Activities

--- app/src/main/java/com/unknown/gkmohit/rpa/StockProfitActivity.java
+++ app/src/main/java/com/unknown/gkmohit/rpa/StockProfitActivity.java
@@ -1,16 +1,61 @@
 package com.unknown.gkmohit.rpa;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class StockProfitActivity extends AppCompatActivity {
+
+    @Bind(R.id.stockTextView) TextView mStockTextView;
+    @Bind(R.id.cashTextView) TextView mCashTextView;
+    @Bind(R.id.estateTextView) TextView mEstateTextView;
+    @Bind(R.id.retirementTextView) TextView mRetirementTextView;
+
+    @Bind(R.id.profitButton)
+    Button mProfitButton;
+
+    @Bind(R.id.goalsButton)
+    Button mGoalsButton;
+
+    @Bind(R.id.ownedButton)
+    Button mOwnedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_profit);
+        ButterKnife.bind(this);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
+        mStockTextView.setTypeface(font);
+        mCashTextView.setTypeface(font);
+        mEstateTextView.setTypeface(font);
+        mRetirementTextView.setTypeface(font);
+
+        mOwnedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ownedPage = new Intent(StockProfitActivity.this, HomePageActivity.class);
+                StockProfitActivity.this.startActivity(ownedPage);
+
+            }
+        });
+
+        mGoalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goalsPage = new Intent(StockProfitActivity.this, StocksGoalsActivity.class);
+                StockProfitActivity.this.startActivity(goalsPage);
+            }
+        });
     }
 
     @Override
