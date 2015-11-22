7fae73ef11eac438dc4edc480ccc60fbde955553
joy99 <sadman.hasan99@live.com>
2015-11-21 19:16:21 -0500

    test

--- app/src/main/java/model/Person.java
+++ app/src/main/java/model/Person.java
@@ -51,5 +51,22 @@ public class Person {
     public Login getLogin() {
         return login;
     }
+
+    public static class PersonAsset {
+
+        private int pid;
+        private String aid;
+        private String bid;
+
+        public PersonAsset(int pid, String aid, String bid) {
+
+            if (pid != 0 && aid != null) {
+                this.pid = pid;
+                this.aid = aid;
+                this.bid = bid;
+            }
+        }
+
+    }
 }
 
