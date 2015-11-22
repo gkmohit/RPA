657cf8cd9f0df6c053615df8c734b898ff6ad3be
joy99 <sadman.hasan99@live.com>
2015-11-22 08:24:46 -0500

    sk

--- app/src/main/java/com/unknown/gkmohit/rpa/MoneyTabActivity.java
+++ app/src/main/java/com/unknown/gkmohit/rpa/MoneyTabActivity.java
@@ -1,6 +1,7 @@
 package com.unknown.gkmohit.rpa;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -32,6 +44,12 @@ public class MoneyTabActivity extends AppCompatActivity {
 
     @Bind(R.id.retirementTextView)
     TextView mRetirementTextView;
+
+    private FrameLayout moneytabBarChart;
+    private BarChart mChart;
+
+    private float[] yData = {9, 13};
+    private String[] xData = {"Income", "Expenditure"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,5 +86,79 @@ public class MoneyTabActivity extends AppCompatActivity {
                 MoneyTabActivity.this.startActivity(nextPage);
             }
         });
+        moneytabBarChart = (FrameLayout) findViewById(R.id.moneytabBarChart);
+        mChart = new BarChart(this);
+        //add pie chart to main layout
+        moneytabBarChart.addView(mChart);
+        moneytabBarChart.setBackgroundColor(Color.argb(250, 242, 242, 242));
+
+        //configure pie chart
+        mChart.setDescription("");
+        mChart.setScaleEnabled(false);
+        mChart.setPinchZoom(true);
+        mChart.setDoubleTapToZoomEnabled(true);
+
+        //chart value listener
+        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry entry, int i, Highlight highlight) {
+                //display msg
+                if (entry == null)
+                    return;
+
+                Toast.makeText(MoneyTabActivity.this,
+                        xData[entry.getXIndex()] + " = " + entry.getVal(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+
+        //add data
+        addData();
+    }
+
+    private void addData() {
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+
+        for (int i = 0; i < yData.length; i++)
+            yVals1.add(new BarEntry(yData[i], i));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < xData.length; i++)
+            xVals.add(xData[i]);
+
+        //create pie data set
+        BarDataSet dataSet = new BarDataSet(yVals1, "Stock");
+        //	dataSet.setSliceSpace(5);
+        //	dataSet.setSelectionShift(5);
+
+        //add many colors
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+        colors.add(Color.rgb(0, 204, 68));
+        colors.add(Color.rgb(230, 0, 0));
+
+
+        dataSet.setColors(colors);
+
+        //instantiate pie data object
+        BarData data = new BarData(xVals, dataSet);
+
+        data.setValueTextSize(16);
+        data.setValueTextColor(Color.BLACK);
+
+        mChart.setData(data);
+
+        //undo highlights
+        mChart.highlightValue(null);
+
+        //update pie chart
+        mChart.invalidate();
+
     }
 }
--- app/src/main/java/com/unknown/gkmohit/rpa/StockProfitActivity.java
+++ app/src/main/java/com/unknown/gkmohit/rpa/StockProfitActivity.java
@@ -1,12 +1,24 @@
 package com.unknown.gkmohit.rpa;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,6 +35,12 @@ public class StockProfitActivity extends AppCompatActivity {
     @Bind(R.id.cashTextView) TextView mCashTextView;
     @Bind(R.id.estateTextView) TextView mEstateTextView;
     @Bind(R.id.retirementTextView) TextView mRetirementTextView;
+    private FrameLayout stockProfitLineChart;
+    private LineChart mChart;
+
+    private float[] yData ={12,13,15,7,30};
+    private String[] xData = {"", "", "", "", ""};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +80,71 @@ public class StockProfitActivity extends AppCompatActivity {
                 StockProfitActivity.this.startActivity(nextPage);
             }
         });
+        stockProfitLineChart = (FrameLayout) findViewById(R.id.stockProfitLineChart);
+        mChart = new LineChart(this);
+        //add pie chart to main layout
+        stockProfitLineChart.addView(mChart);
+        stockProfitLineChart.setBackgroundColor(Color.argb(250, 242, 242, 242));
+
+        //configure pie chart
+
+        mChart.setDescription("");
+        mChart.setDoubleTapToZoomEnabled(false);
+        mChart.setPinchZoom(false);
+
+        //chart value listener
+        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry entry, int i, Highlight highlight) {
+                //display msg
+                if (entry == null)
+                    return;
+
+                Toast.makeText(StockProfitActivity.this,
+                        xData[entry.getXIndex()] + " = " + entry.getVal(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+        //add data
+        addData();
     }
 
+    private void addData() {
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        for(int i =0; i< yData.length; i++)
+            yVals1.add(new Entry(yData[i], i));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < xData.length; i++)
+            xVals.add(xData[i]);
+
+        //create pie data set
+        LineDataSet dataSet = new LineDataSet(yVals1, "Stock");
+        dataSet.setCircleSize(7);
+
+        dataSet.setColor(Color.RED);
+        dataSet.setLineWidth(3);
+        //instantiate pie data object
+        LineData data = new LineData(xVals, dataSet);
+
+        data.setValueTextSize(15);
+        data.setValueTextColor(Color.BLACK);
+
+        mChart.setData(data);
+
+        //undo highlights
+        mChart.highlightValue(null);
+
+        //update pie chart
+        mChart.invalidate();
+
+    }
 }
