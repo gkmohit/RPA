dce2bb4072789f2a212063df213bb61ffe7c5657
Mohit Kishore <gkmohit@gmail.com>
2015-11-22 04:18:57 -0500

    Mohit Added stock goals activity

--- app/src/main/java/com/unknown/gkmohit/rpa/MoneyTabActivity.java
+++ app/src/main/java/com/unknown/gkmohit/rpa/MoneyTabActivity.java
@@ -1,37 +1,20 @@
 package com.unknown.gkmohit.rpa;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MoneyTabActivity extends AppCompatActivity {
 
+    Bind(R.id.)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money_tab);
+        ButterKnife.bind(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_money_tab, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
